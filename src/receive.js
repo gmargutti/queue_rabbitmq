71c2b9a7e0c8085dc26a80fbf93c1bd8a647d366
Gustavo Margutti <gustavo.margutti@hotmail.com>
2020-12-05 15:47:41 -0300

    Removed comment lines

--- src/receive.js
+++ src/receive.js
@@ -20,23 +20,8 @@ setTimeout(() => {
             });
     
             console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-    
-            // setInterval(() => {
-            //     channel.get(queue, {}, function(err, msg) {
-            //         channel.ack(msg)
-            //         console.log(" [x] Received %s", msg ? msg.content.toString() : '');
-            //     }, {
-            //         noAck: true
-            //     });
-            // }, 1000)
 
             channel.consume(queue, (msg) => {
-                // const random = Math.random() * (5000 - 1000) + 1000;
-                // const end = Date.now() + random;
-                // while (Date.now() < end) {
-                //     const doSomethingHeavyInJavaScript = 1 + 2 + 3;
-                // }
-                // Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, random);
                 const { id: count } = JSON.parse(msg.content.toString())
                 axios({
                     url: `${process.env.API_HOST}/queue/save_db`,
